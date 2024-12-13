ee0d15d20d573293ed467722650f08fc443fc001
ixedasan <pandikgenshin@gmail.com>
2024-12-13 22:55:20 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+export default App
