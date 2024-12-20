560275f9a9e4f692b10de8f73011b63b46d4dff8
ixedasan <pandikgenshin@gmail.com>
2024-12-20 18:13:35 +0200

    feat(search): add city search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,17 @@ import { Link } from 'react-router-dom'
 
 import Logo from './common/Logo'
 import ThemeSwitcher from './common/ThemeSwitcher'
+import SearchCity from './features/city/Search'
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-10 border-b bg-background/50 py-2 backdrop-blur-sm">
       <div className="container mx-auto flex h-16 items-center justify-between">
-        <div>
-          <Link to="/">
-            <Logo />
-          </Link>
-        </div>
+        <Link to="/">
+          <Logo />
+        </Link>
         <div className="flex gap-4">
-          {/* TODO: search */}
+          <SearchCity />
           <ThemeSwitcher />
         </div>
       </div>
